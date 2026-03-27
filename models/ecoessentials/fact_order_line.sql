bca8bb694fe22436f786c9a7f63e075c6895e811
Jacob Jessop <brockjessop53@gmail.com>
2026-03-27 01:37:21 +0000

    Final Project Deliverable 2

--- models/ecoessentials/fact_order_line.sql
+++ models/ecoessentials/fact_order_line.sql
@@ -1,8 +1,7 @@
 {{ config(
     materialized = 'table',
     schema = 'dw_eco_essential'
-    )
-}}
+) }}
 
 SELECT
     o.order_id,
@@ -14,10 +13,16 @@ SELECT
     ol.quantity,
     ol.discount,
     ol.price_after_discount
-FROM {{source('eco_essential_landing', 'orders')}} o
-INNER JOIN {{source('eco_essential_landing', 'ORDER_LINE')}} ol ON o.order_id = ol.order_id
-INNER JOIN {{source('eco_essential_landing', 'product')}} ps ON ps.product_id = ol.product_id
-INNER JOIN {{ref('dim_customer')}} cu ON cu.customer_id = o.customer_id
-INNER JOIN {{ref('dim_product')}} p ON p.product_id = ol.product_id
-INNER JOIN {{ref('dim_promotional_campaign')}} c ON c.campaign_id = ol.campaign_id
-INNER JOIN {{ref('dim_date')}} d ON d.date = CAST(o.order_timestamp AS DATE)
+FROM {{ source('eco_essential_landing', 'orders') }} o
+INNER JOIN {{ source('eco_essential_landing', 'ORDER_LINE') }} ol
+    ON o.order_id = ol.order_id
+INNER JOIN {{ source('eco_essential_landing', 'product') }} ps
+    ON ps.product_id = ol.product_id
+LEFT JOIN {{ ref('dim_customer') }} cu
+    ON cu.customer_id = o.customer_id
+INNER JOIN {{ ref('dim_product') }} p
+    ON p.product_id = ol.product_id
+LEFT JOIN {{ ref('dim_promotional_campaign') }} c
+    ON c.campaign_id = ol.campaign_id
+INNER JOIN {{ ref('dim_date') }} d
+    ON d.date = CAST(o.order_timestamp AS DATE)
